2e2ba5022be6ae6cc2638db8c4bd2da3a3c9d3ef
Taylor Lovett <tlovett88@gmail.com>
2019-03-17 23:07:42 -0400

    Fix autoload

--- src/subprocesses/createnetwork.php
+++ src/subprocesses/createnetwork.php
@@ -7,7 +7,13 @@
 
 namespace WPInstructions;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+if ( file_exists( __DIR__ . '/../../vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/../../vendor/autoload.php';
+} elseif ( file_exists( __DIR__ . '/../../../../autoload.php' ) ) {
+	require_once __DIR__ . '/../../../../autoload.php';
+} else {
+	exit( 1 );
+}
 
 define( 'WP_INSTALLING', true );
 
